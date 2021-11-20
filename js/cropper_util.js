85e6385c58475b3ffdfe5abdd9fbafbcd0a7be32
ab997 <58363787+ab997@users.noreply.github.com>
2021-11-20 17:37:21 +0100

    so far so good, i like it

--- js/cropper_util.js
+++ js/cropper_util.js
@@ -1,10 +1,14 @@
 window.addEventListener('DOMContentLoaded', function () {
-    var avatar = document.getElementById('avatar');
+    var initialAvatarURL;
+    var avatar = document.getElementById('imageResult');
+    initialAvatarURL = avatar.src;
     var image = document.getElementById('image');
     var input = document.getElementById('upload');
+    var myText = document.getElementById("myText");
     var $progress = $('.progress');
     var $progressBar = $('.progress-bar');
     var $alert = $('.alert');
+    console.log($alert);
     var $modal = $('#modal');
     var cropper;
 
@@ -41,24 +45,29 @@ window.addEventListener('DOMContentLoaded', function () {
       cropper = new Cropper(image, {
         aspectRatio: 1,
         viewMode: 3,
+        minCropBoxWidth: 400,
+        minCropBoxHeight: 400,
       });
     }).on('hidden.bs.modal', function () {
       cropper.destroy();
       cropper = null;
     });
 
+    document.getElementById('close').addEventListener('click', function () {
+      avatar.src = initialAvatarURL;
+    });
+
     document.getElementById('crop').addEventListener('click', function () {
-      var initialAvatarURL;
       var canvas;
 
       $modal.modal('hide');
 
       if (cropper) {
         canvas = cropper.getCroppedCanvas({
-          width: 160,
-          height: 160,
+          width: 400,
+          height: 400,
+          
         });
-        initialAvatarURL = avatar.src;
         avatar.src = canvas.toDataURL();
         $progress.show();
         $alert.removeClass('alert-success alert-warning');
@@ -90,15 +99,19 @@ window.addEventListener('DOMContentLoaded', function () {
             },
 
             success: function () {
-              $alert.show().addClass('alert-success').text('Upload success');
+              initialAvatarURL = avatar.src;
+              $alert.show().addClass('alert-success text-center').text('Nalaganje uspešno');
             },
 
             error: function () {
               avatar.src = initialAvatarURL;
-              $alert.show().addClass('alert-warning').text('Upload error');
+              $alert.show().addClass('alert-warning text-center').text('Napaka pri nalaganju na strežnik');
             },
 
             complete: function () {
+              if (myText) {
+                myText.remove();
+              }
               $progress.hide();
             },
           });
--- js/image_upload.js
+++ js/image_upload.js
@@ -27,27 +27,8 @@ var input = document.getElementById( 'upload' );
 var infoArea = document.getElementById( 'upload-label' );
 
 input.addEventListener( 'change', showFileName );
-input.addEventListener( 'change', showCropper );
 function showFileName( event ) {
   var input = event.srcElement;
   var fileName = input.files[0].name;
   infoArea.textContent =  fileName;
 };
-function showCropper( event ) {
-    const image = document.getElementById('imageResult');
-    console.log("test");
-    console.log(image);
-    const cropper = new Cropper(image, {
-        aspectRatio: 1 / 1,
-        crop(event) {
-          console.log(event.detail.x);
-          console.log(event.detail.y);
-          console.log(event.detail.width);
-          console.log(event.detail.height);
-          console.log(event.detail.rotate);
-          console.log(event.detail.scaleX);
-          console.log(event.detail.scaleY);
-        },
-      });
-    console.log(cropper);
-  }
