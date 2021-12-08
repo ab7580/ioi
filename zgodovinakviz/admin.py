7b1627d89911e96d635e903d33e90bd9b42a25b3
es6317 <erika.five123@gmail.com>
2021-12-08 18:28:54 +0100

    oops

--- zgodovinakviz/admin.py
+++ zgodovinakviz/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Vprasanje
 from .models import Odgovor
+from .models import Level
 
 admin.site.register(Vprasanje)
 admin.site.register(Odgovor)
+admin.site.register(Level)
